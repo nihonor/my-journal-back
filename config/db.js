5dcc3aa4b3adaa49ea7f7dd95bf0ed9576c89f84
nihonor <niyitanganihonor@gmail.com>
2026-01-05 16:12:55 +0200

    feat: set up MongoDB connection

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 module.exports = function () {
-    mongoose.connect(process.env.MONGO_URI)
+    mongoose.connect("mongodb + srv://niyitanganihonor:honore@cluster0.wskc7ct.mongodb.net/?appName=Cluster0")
         .then(() => console.log("Connected to MongoDB"))
         .catch((err) => console.log(err));
 }
