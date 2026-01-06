dad61b613d47510ec4b9cc9b3d2c50f3ce4f2478
nihonor <niyitanganihonor@gmail.com>
2026-01-06 20:35:17 +0200

    feat: add MongoDB connection logic using Mongoose

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 module.exports = function () {
-    mongoose.connect("mongodb + srv://niyitanganihonor:honore@cluster0.wskc7ct.mongodb.net/?appName=Cluster0")
+    mongoose.connect("mongodb+srv://niyitanganihonor:honore@cluster0.wskc7ct.mongodb.net/?appName=Cluster0")
         .then(() => console.log("Connected to MongoDB"))
         .catch((err) => console.log(err));
 }
